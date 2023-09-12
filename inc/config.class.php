f57d294f06cc9ed6b735f4157b1076cca1575073
minzords <florian.blanchet@itsm-ng.com>
2023-09-12 12:05:04 +0200

    feat(*): add {add,search} button

--- inc/config.class.php
+++ inc/config.class.php
@@ -13,6 +13,8 @@ class PluginRssfeedConfig extends CommonDBTM {
         $menu['title'] = __("RSS feed", 'rssfeed');
         $menu['page'] = "/plugins/rssfeed/front/rssfeed.php";
         $menu['icon']  = "fas fa-rss";
+        $menu['links']['search'] = '/plugins/rssfeed/front/rssfeed.php';
+        $menu['links']['add'] = '/plugins/rssfeed/front/rssfeed.form.php';
 
         return $menu;
     }
